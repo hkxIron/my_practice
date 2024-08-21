140de882f5b2161ed46c1ee9cebb56e08be62e26
hukexin <hukexin@xiaomi.com>
2024-08-21 10:04:50 +0800

    add

--- rope/test_rope.py
+++ rope/test_rope.py
@@ -1,50 +1,55 @@
 import torch
 from utils.util import *
 
+# blog: https://kexue.fm/archives/8265
 def sin_cos_position_embedding(batch_size,
                                head_num,
                                max_seq_len,
                                head_dim):
     # position: (max_seq_len, 1)
     position = torch.arange(0, max_seq_len, dtype=torch.float).unsqueeze(-1)
-    # dim_index: [output_dim//2], 两两一组，分别代表复数实部和虚部
+    # dim_index: [head_dim//2], 两两一组，分别代表复数实部和虚部
     dim_index = torch.arange(0, head_dim // 2, dtype=torch.float)  # 即公式里的i, i的范围是 [0,d/2]
     # theta(i) = 10000^(-2i/d), theta(0)=10000^0=1, theta(d/2)=10000^(-d/d)=1/10000
     theta = torch.pow(10000, -2 * dim_index / head_dim)
 
     # (max_seq_len, head_dim//2)
     # 即公式里的：pos / (10000^(2i/d)), position_embed(m) = e^(j*m*theta) = e^(j*m/[10000^(2i/d)]),其中j为虚数单位
-    embeddings = position * theta
+    position_embeddings = position * theta
 
     # [max_seq_len, head_dim//2]
     # =>
     # [max_seq_len, head_dim//2, 2]
-    embeddings = torch.stack([torch.sin(embeddings),
-                                      torch.cos(embeddings)], dim=-1) # 在最后一维上stack,注意不是torch.cat
+    position_embeddings = torch.stack([torch.sin(position_embeddings),
+                                      torch.cos(position_embeddings)], dim=-1) # 在最后一维上stack,注意不是torch.cat
 
     # [batch, head_num, max_len, head_dim//2, 2]
     # torch中的repeat就是(整体复制)铺地砖样复制，比如[1,2,3] repeat(2)变成[1,2,3, 1,2,3]
-    embeddings = embeddings.repeat((batch_size, head_num, *([1] * len(embeddings.shape))))  # 在batch,head_num两个维度重复，其他维度都是1不重复
+    position_embeddings = position_embeddings.repeat((batch_size, head_num, *([1] * len(position_embeddings.shape))))  # 在batch,head_num两个维度重复，其他维度都是1不重复
 
     # [batch, head_num, max_len, head_dim//2, 2], 最后一维(sin, cos)
     # [batch, head_num, max_len, head_dim]
     # reshape后：偶数sin, 奇数cos了
-    embeddings = torch.reshape(embeddings, (batch_size, head_num, max_seq_len, head_dim))
+    position_embeddings = torch.reshape(position_embeddings, (batch_size, head_num, max_seq_len, head_dim))
     """
-    embeddings: 在最后一维head_dim维
+    position_embeddings: 在最后一维head_dim维
     [batch, head_num, max_len, head_dim]
     示例数据： 
-    [:, :, m, [ sin(m*theta0),
-                cos(m*theta0),
-                sin(m*theta1),
-                cos(m*theta1),
-                ...
-                sin(m*theta(head_dim//2)),
-                cos(m*theta(head_dim//2))
-                ]]
+    [:, :, max_len=m, [ sin(m*theta0),
+                        cos(m*theta0),
+                        sin(m*theta1),
+                        cos(m*theta1),
+                        ...
+                        sin(m*theta(head_dim//2)),
+                        cos(m*theta(head_dim//2))
+                        ]]
     """
-    return embeddings
+    return position_embeddings
 
+"""
+对q,k分别在复数空间下进行m*theta旋转
+这样可以使它们在attention时具有相对位置信息
+"""
 def RoPE(q:torch.Tensor, k:torch.Tensor):
     # q,k: (batch, head_num, max_len, head_dim)
     batch_size, head_num, max_len, head_dim = q.shape
@@ -54,14 +59,14 @@ def RoPE(q:torch.Tensor, k:torch.Tensor):
     # 其值为： e^(j*m/[10000^(2i/head_dim)]), i为索引，2i为偶数, m为位置, 最后一维(sin, cos)
     """
     pos_emb:
-    [:, :, m, [ sin(m*theta0), # 0
-                cos(m*theta0), # 1
-                sin(m*theta1), # 2
-                cos(m*theta1), # 3
-                ...
-                sin(m*theta(head_dim//2)),
-                cos(m*theta(head_dim//2))
-                ]]
+    [:, :, max_len=m, [ sin(m*theta0), # 0
+                        cos(m*theta0), # 1
+                        sin(m*theta1), # 2
+                        cos(m*theta1), # 3
+                        ...
+                        sin(m*theta(head_dim//2)),
+                        cos(m*theta(head_dim//2))
+                        ]]
     """
     pos_emb = sin_cos_position_embedding(batch_size, head_num, max_len, head_dim).to(q.device)
 
@@ -70,23 +75,23 @@ def RoPE(q:torch.Tensor, k:torch.Tensor):
     # pos_emb中head_dim维的偶数列是sin，奇数列是cos
     """
     sin_pos: 
-    [:, :, m, [ sin(m*theta0), # 0
-                sin(m*theta0), # 1
-                sin(m*theta1), # 2
-                sin(m*theta1), # 3
-                ...
-                sin(m*theta(head_dim//2)),
-                sin(m*theta(head_dim//2))
-                ]]
+    [:, :, max_len=m, [ sin(m*theta0), # 0
+                        sin(m*theta0), # 1
+                        sin(m*theta1), # 2
+                        sin(m*theta1), # 3
+                        ...
+                        sin(m*theta(head_dim//2)),
+                        sin(m*theta(head_dim//2))
+                        ]]
     cos_pos: 
-    [:, :, m, [ cos(m*theta0), # 0
-                cos(m*theta0), # 1
-                cos(m*theta1), # 2
-                cos(m*theta1), # 3
-                ...
-                cos(m*theta(head_dim//2)),
-                cos(m*theta(head_dim//2))
-                ]]
+    [:, :, max_len=m, [ cos(m*theta0), # 0
+                        cos(m*theta0), # 1
+                        cos(m*theta1), # 2
+                        cos(m*theta1), # 3
+                        ...
+                        cos(m*theta(head_dim//2)),
+                        cos(m*theta(head_dim//2))
+                        ]]
     
     """
     sin_pos = pos_emb[..., ::2].repeat_interleave(2, dim=-1)  # 将偶数列信息抽取出来也就是sin 拿出来并复制
@@ -96,22 +101,22 @@ def RoPE(q:torch.Tensor, k:torch.Tensor):
     """
     q:[batch,  head, max_len, head_dim]
     数据如下:
-    [:,:,m, [q0,
-             q1,
-             q2,
-             ...
-             q(head_dim)]
+    [:,:,max_len=m, [q0,
+                     q1,
+                     q2,
+                     ...
+                     q(head_dim)]
     ] 
     q_imag:
-    [:,:,m, [-q1,
-             q0,
-             -q3,
-             q2,
-             -q5,
-             q4,
-             ...
-             -q(head_dim-1),
-             q(head_dim)]
+    [:,:,max_len=m, [-q1,
+                     q0,
+                     -q3,
+                     q2,
+                     -q5,
+                     q4,
+                     ...
+                     -q(head_dim-1),
+                     q(head_dim)]
     ] 
     """
     q_imag = torch.stack([-q[..., 1::2],  # 只取奇数位然后取负
@@ -121,9 +126,19 @@ def RoPE(q:torch.Tensor, k:torch.Tensor):
 
     # 计算Rope(q)=q*pos(m, head_dim), *对应位置相乘
     """
-    RoPE
-   =  [q0, q1, q2, ..., q(d-2), q(d-1)] .* [cos(m*theta0), cos(m*theta0),cos(m*theta1), ..., cos(m*theta(head_dim/2-1)),cos(m*theta(head_dim/2))]  .*代表逐元素相乘
-    + [-q1,q0,-q3, ..., -q(d-1), q(d-2)] .* [sin(m*theta0), sin(m*theta0),sin(m*theta1), ..., sin(m*theta(head_dim/2-1)),sin(m*theta(head_dim/2))]
+    RoFormer中的Rope, 即RoFormer中是将相邻位置(q0,q1)作为复数的实部与虚部 , dim=head_dim
+    = [q0, q1, q2, ..., q(d-2), q(d-1)] .* [cos(m*theta0), cos(m*theta0), cos(m*theta1),cos(m*theta1), ..., cos(m*theta(head_dim/2)),cos(m*theta(dim/2))]  .*代表逐元素相乘
+    + [-q1,q0,-q3, ...,-q(d-1), q(d-2)] .* [sin(m*theta0), sin(m*theta0), sin(m*theta1),sin(m*theta1), ..., sin(m*theta(head_dim/2)),sin(m*theta(dim/2))]
+    = [
+       q0*cos(m*theta0)-q1*sin(m*theta0),
+       q1*cost(m*theta0)+q0*sin(m*theta0),
+       q2*cos(m*theta1)-q3*sin(m*theta1),
+       q3*cost(m*theta1)+q2*sin(m*theta1),
+       ...
+      q(d-2)*cos(m*theta(dim/2))-q(d-1)*sin(m*theta(dim/2)),
+      q(d-1)*cos(m*theta(dim/2))-q(d-2)*sin(m*theta(dim/2))
+    ]
+    
     """
     q = q * cos_pos + q_imag * sin_pos
 
