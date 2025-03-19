33a07e24c4cef6a238d3723a08ff21855a662aea
hukexin <hukexin@xiaomi.com>
2025-03-19 16:02:36 +0800

    add

--- utils/shell_utils.sh
+++ utils/shell_utils.sh
@@ -14,7 +14,10 @@ show_run_time(){
 test_max_gpu_num(){
   local max_gpu_num=$1 # 最少多少个gpu
   echo "限制最大gpu数量：$max_gpu_num"
-  # 有时gpu里的总线会占用一点点,但显存并未占用
+  # 有时gpu里的显存有一定占用,比较小,但也可以训练
+  # 或者用以下命令更好
+  # nvidia-smi --query-gpu=index,memory.used --format=csv 
+  # nvidia-smi --query-gpu=index,memory.used --format=csv | column -s ', ' -t
   device_list=$(nvidia-smi|grep "NVIDIA-SMI" -A37|awk -F'|' '{ print($2,"|",$3) }'|grep -E '\b[0-9]{1,3}MiB / ' -B1|grep "NVIDIA"|awk -F' ' '{print $1}'|head -n $max_gpu_num|tr '\n' ','|sed 's/,$//')
   gpu_num=$(echo ${device_list}|awk -F',' '{print NF}')
   echo "gpu ids:$device_list size:${gpu_num}"
